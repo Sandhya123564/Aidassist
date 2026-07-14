7f5080834a0594cedb5b581597c67f465daa4bbe
sandhya <sandhyadsan03@gmail.com>
2026-07-14 15:33:24 +0530

    Fix rag_service syntax

--- backend/rag_service.py
+++ backend/rag_service.py
@@ -1,20 +1,23 @@
 from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
+
 print("Loading embeddings...")
 
 CHROMA_DB = "chroma_db"
 
 embeddings = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
-)\
+)
+
 print("Embeddings loaded.")
 
-print("Opening Chroma DB...")-
+print("Opening Chroma DB...")
 
 db = Chroma(
     persist_directory=CHROMA_DB,
     embedding_function=embeddings
 )
+
 print("Chroma DB opened.")
 
 def search_documents(query):
