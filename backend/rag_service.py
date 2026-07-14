8cebcd7a34cb378c8e6602f46b9b87f2fabcd03f
sandhya <sandhyadsan03@gmail.com>
2026-07-14 15:19:17 +0530

    Add debug prints

--- backend/rag_service.py
+++ backend/rag_service.py
@@ -1,16 +1,21 @@
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
+print("Loading embeddings...")
 
 CHROMA_DB = "chroma_db"
 
 embeddings = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
+)\
+print("Embeddings loaded.")
+
+print("Opening Chroma DB...")-
 
 db = Chroma(
     persist_directory=CHROMA_DB,
     embedding_function=embeddings
 )
+print("Chroma DB opened.")
 
 def search_documents(query):
     docs = db.similarity_search(query, k=3)
